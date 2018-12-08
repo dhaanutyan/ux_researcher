c003621c51d27a54432f1b99a393983b3a0bf436
VladyslavO <2964824+VladyslavO@users.noreply.github.com>
2018-12-08 05:39:59 +0200

    Added ability to drag and drop steps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery3
+//= require interact
 //= require_tree .
 
 $(document).ready(function(){
@@ -23,5 +24,42 @@ $(document).ready(function(){
     var stepNumber = parseInt(lastStep.data("step-number")) + 1;
 
     lastStep.after('<div class="step" data-step-number=' + stepNumber + '>' + stepNumber + '</div>');
+  });
+
+  interact(".drag-drop").draggable({
+    inertia: true,
+    autoScroll: true,
+    onmove: function(e){
+      var target = $(e.target);
+      var x = parseFloat(target.data("x") || 0) + e.dx;
+      var y = parseFloat(target.data("y") || 0) + e.dy;
+
+      target.css("transform", "translate(" + x + "px," + y + "px)");
+      target.data("x", x);
+      target.data("y", y);
+    },
+    onend: function(e){
+      if(typeof e.dropzone === "undefined")
+      {
+        var target = $(e.target);
+
+        target.css("transform", "translate(0, 0)");
+        target.data("x", 0);
+        target.data("y", 0);
+      }
+    }
+  });
+
+  interact(".drop-zone").dropzone({
+    overlap: 0.75,
+    ondrop: function(e){
+      var target = $(e.target);
+      var relatedTarget = $(e.relatedTarget);
+      var x = parseFloat(relatedTarget.data("x") || 0) + target.offset().left - relatedTarget.offset().left;
+      var y = parseFloat(relatedTarget.data("y") || 0) + target.offset().top - relatedTarget.offset().top;
+      relatedTarget.css("transform", "translate(" + x + "px," + y + "px)");
+      relatedTarget.data("x", x);
+      relatedTarget.data("y", y);
+    }
   });
 });
